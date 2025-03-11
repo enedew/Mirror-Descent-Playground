0b6fba58ce22af10e2e48b68071c0ef980328d2a
p56273gd <gabriel.downes@student.manchester.ac.uk>
2025-03-11 18:39:11 +0000

    Normalize line endings

--- experiment0.py
+++
@@ -1,6 +0,0 @@
-from Experiment import ExperimentMD
-import torch
-
-euclidean_test = ExperimentMD(lambda X: 10*5 + (X**2 -torch.cos(2*torch.pi*X)))
-print(euclidean_test.run_experiment_minimise(5.0, 100, 0.001))
-euclidean_test.create_minimisation_iteration_graph()
